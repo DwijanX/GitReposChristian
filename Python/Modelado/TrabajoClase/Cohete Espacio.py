a22feb01ee5837032739145a01bb73c999142eef
DwijanX <70019894+DwijanX@users.noreply.github.com>
2022-03-02 17:53:21 -0400

    Cohete Espacio

--- Python/Modelado/TrabajoClase/Cohete Espacio.py
+++ Python/Modelado/TrabajoClase/Cohete Espacio.py
@@ -9,13 +9,19 @@ import numpy
 import matplotlib.pyplot as pl
 
 #ci=>[Velocidad,[Posicion]]
+def dvdtComponent(MObj1,Pos1To2):
+    return G*MObj1*Pos1To2/numpy.linalg.norm(Pos1To2)**3
+
+
+
 def Cohete(t,ci,G,Mt,ML,PosTierra,PosLuna):
     NaveTierra=PosTierra-ci[1]
     DistanciaNaveTierra=numpy.linalg.norm(PosTierra-ci[1])
+    LunaTierra=PosLuna-ci[1]
     DistanciaNaveLuna=numpy.linalg.norm(PosLuna-ci[1]).real
-    a=G*Mt*NaveTierra/DistanciaNaveTierra**3
-    b=(G*ML/(DistanciaNaveLuna**2))*((PosLuna-ci[1])/DistanciaNaveLuna)
-    dvdt=a+b
+    a=dvdtComponent(Mt,NaveTierra)#G*Mt*NaveTierra/DistanciaNaveTierra**3
+    b=dvdtComponent(Mt,NaveTierra)#G*Mt*LunaTierra/DistanciaNaveLuna**3
+    dvdt=a+b#GetVelo(ci[1],PosTierra,PosLuna)
     dxdt=ci[0]
     return numpy.array([dvdt,dxdt])
 
@@ -27,17 +33,24 @@ PosicionLuna=numpy.array([0,DTierraLuna,0],dtype=numpy.double)
 PosicionCohete=numpy.array([DTierraCohete,0,0],dtype=numpy.double)
 G=6.667*(10)**(-11)# N*M^2/kg^2
 MasaTierra=5.972*(10**24) #kg
-MasaLuna=200.349*(10**22) #kg
+MasaLuna=7.349*(10**22) #kg
 
-def GetVelo(MasaObj,ArrayDistance):
-    a=sqrt(G*MasaObj/numpy.linalg.norm(ArrayDistance)).real
+def GetVeloInicial(MasaObj,ArrayDistance):
+    a=sqrt(G*MasaObj/numpy.linalg.norm(ArrayDistance))
     return a
+def GetVelo(PosCohete,posTierra,PosLuna):
+    PosCoheteTierra = posTierra-PosCohete
+    PosNaveLuna=PosLuna-PosCohete
+    a=+dvdtComponent(MasaLuna,PosNaveLuna)
+    b=+dvdtComponent(MasaTierra,PosCoheteTierra)
+    return(a+b)
+
 
 MagnitudVelocidad = sqrt(G*MasaTierra/DTierraCohete).real
 
-MagnitudVelocidadNave = GetVelo(MasaTierra,(PosicionTierra-PosicionCohete)) 
-MagnitudVelocidadNave2 = GetVelo(MasaLuna,(PosicionLuna-PosicionCohete))
-VelocidadNave = numpy.array([-MagnitudVelocidadNave2,MagnitudVelocidadNave,0])
+MagnitudVelocidadNave = GetVeloInicial(MasaTierra,(PosicionTierra-PosicionCohete)) 
+MagnitudVelocidadNave2 = GetVeloInicial(MasaLuna,(PosicionLuna-PosicionCohete))
+VelocidadNave = numpy.array([0,MagnitudVelocidadNave,0])
 
 ci=numpy.array([VelocidadNave,PosicionCohete])
 T = 2*numpy.pi*DTierraCohete/MagnitudVelocidad
@@ -45,17 +58,18 @@ dt = T/100
 
 
 Y,tiempo=heuz(0,10*T,ci,dt,Cohete,G,MasaTierra,MasaLuna,PosicionTierra,PosicionLuna)
-#"""
+"""
 pl.plot(tiempo,Y[:,0][:,0],label="Velocidad en X",color="red")
 pl.plot(tiempo,Y[:,0][:,1],label="Velocidad en Y",color="blue")
 pl.plot(tiempo,Y[:,0][:,2],label="Velocidad en Z",color="black")
+"""
+
+
 #"""
-
-
-"""
-pl.plot(tiempo,Y[:,1][:,0],label="distancia recorrida en X",color="red")
+"""pl.plot(tiempo,Y[:,1][:,0],label="distancia recorrida en X",color="red")
 pl.plot(tiempo,Y[:,1][:,1],label="distancia recorrida en Y",color="blue")
-pl.plot(tiempo,Y[:,1][:,2],label="distancia recorrida en Z",color="black")
-"""
+pl.plot(tiempo,Y[:,1][:,2],label="distancia recorrida en Z",color="black")"""
+pl.plot(Y[:,1][:,0],Y[:,1][:,1],label="distancia recorrida en Z",color="black")
+#"""
 pl.legend()
 pl.grid()
