a6b17d83f36fbdf51f591433f9cf27b5fd584d9e
DwijanX <70019894+DwijanX@users.noreply.github.com>
2022-03-30 17:33:45 -0400

    30/3

--- Python/Modelado/TrabajoClase/ModeloMultiData.py
+++ Python/Modelado/TrabajoClase/ModeloMultiData.py
@@ -1,15 +1,13 @@
 #%%
+from statistics import mean
 import numpy
 import matplotlib.pyplot as pl
 
 
-A=numpy.array([[1402,191],[1371,821],[949,1437],[147,1448]])
+data=numpy.loadtxt('../Files/datosMulti.txt',delimiter=',')
+x=data[:,0:-1]
+y=data[:,-1]
 
-
-
-data=numpy.loadtxt('../Files/datos01.txt',delimiter=',')
-x=data[:,0]
-y=data[:,1]
 
 
 
@@ -18,38 +16,43 @@ class Regression:
         self__X=None
         self__Y=None
         self_theta=None
-        len=None
 
     def fit(self,x,y):
-        self.__X=x
-        self.__Y=y
-        self.__len=len(x)
-        self.theta=numpy.array([-2,3.4])
+        m,n=x.shape
+        self.__X=numpy.ones((m,n+1))
+        self.__X[:,1:]=x
+        self.__Y=y.reshape(-1,1)
+        
+        self.theta=numpy.zeros(n+1)
 
     def get_h(self):
-        return self.theta[0]+self.theta[1]*self.__X
+        theta=self.theta.reshape(-1,1)
+        h=self.__X.dot(theta)
+        #print(self.__X)
+        return h
     def Graph(self,modelo=False):
         pl.scatter(self.__X,self.__Y)
         if modelo:
             pl.plot(self.__X,self.get_h(),color="red")
         pl.grid()
-    def get_j(self,theta):
-        h = theta[0] + theta[1] * self.__X
+    def get_j(self):
+        m,n = self.__X.shape
+        h = self.get_h()
         error = h - self.__Y
-        j = 1 / (2 * self.__len)* error ** 2
+        j = 1 / (2 * m)* error ** 2
         return j.sum()
     def get_gradiente(self, theta):
-        h = theta[0] + theta[1] * self.__X
+        m,n = self.__X.shape
+        h = self.get_h()
         error = h - self.__Y
-        djdt0 = 1 / self.__len * error
-        djdt1 = 1 / self.__len * error * self.__X
-        return numpy.array([djdt0.sum(), djdt1.sum()])
+        djdt = 1 / m * error.T.dot(self.__X)
+        return djdt.ravel()
 
     def descenso_gradiente(self, alpha, epsilon=1.0e-6):
         js = []
         theta = self.theta
         while True:
-            js.append(self.get_j(self.theta))
+            js.append(self.get_j())
             theta = theta - alpha * self.get_gradiente(theta)
             if abs(self.get_j(theta) - js[-1]) < epsilon:
                 break
@@ -60,16 +63,20 @@ class Regression:
         js = []
         theta = self.theta
         for i in range(itera):
-            js.append(self.get_j(self.theta))
+            js.append(self.get_j())
             theta = theta - alpha * self.get_gradiente(theta)
-            self.theta = theta
+        self.theta = theta
         return numpy.array(js)
-    
+    def normalizar(self):
+        Mean=self.__X.mean(1)
+        dsvStd=self.__X.std(1)
+        for i in range(len(Mean)):
+            self.__X[:,1]=(Mean-self.__X)/dsvStd
+        print(ans)
+        return ans
+
 
 r=Regression()
 r.fit(x,y)
-theta=numpy.array([0,0.005])
-r.theta=theta
-historial=r.descenso_gradiente(0.01)
-r.Graph(True)
-#r.Graph(True)
+historial=r.descenso_gradiente2(0.01,400)
+print(r.normalizar())
