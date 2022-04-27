fe0ba7a133a0a8218713dd6559d37ec99f119f75
DwijanX <70019894+DwijanX@users.noreply.github.com>
2022-04-27 17:26:22 -0400

    2704

--- Python/Modelado/TrabajoClase/Red_Neuronal.py
+++ Python/Modelado/TrabajoClase/Red_Neuronal.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy
+import scipy
 
 
 class RedNeuronal:
@@ -21,7 +22,9 @@ class RedNeuronal:
     def sigmoide(self, z):
         s = 1 / (1 + numpy.exp(-z))
         return s
-
+    def inicializar(self,epsilon=0.12):
+        self.theta1=numpy.random.rand(self.capa2,(self.capa1+1))*2*epsilon-epsilon
+        self.theta2=numpy.random.rand(self.capa3,(self.capa2+1))*2*epsilon-epsilon
     def cargar_parametros(self, archivo):
         param = h5py.File(archivo, 'r')
         self.theta1 = param['Theta1'][:]
@@ -33,3 +36,54 @@ class RedNeuronal:
         a2 = numpy.concatenate([numpy.ones((a2.shape[0], 1)), a2], axis=1)
         a3 = self.sigmoide(a2.dot(self.theta2.T))
         return a3.argmax()
+    def derivadaSigmoide(self,a):
+        return self.sigmoide(a)*(1-self.sigmoide(a))
+    def getCrossedEntropy(self,t):
+        t1=numpy.reshape(t[0:(self.capa1+1)*self.capa2],(self.capa2,self.capa1+1))
+        t2=numpy.reshape(t[(self.capa1+1)*self.capa2:],(self.capa3,self.capa2+1))
+        m,n=self.X
+        a1=numpy.concatenate([numpy.ones((m,1)),self.X],axis=1)
+        a2=self.sigmoide(a1.dot(t1.T))
+        a2=numpy.concatenate([numpy.ones((m,1)),a2],axis=1)
+        h=self.sigmoide(a2.dot(t2.T))
+        yVec=numpy.eye(self.capa3)[self.y.reshape(-1)]
+        #para reg
+        param_reg=self.lambda_/(2*m)*(numpy.sum(numpy.square(t1[:,1:]))+numpy.sum(numpy.square(t2[:,1:])))
+        #entropua cruzada
+        j=1 / m * yVec.dot(numpy.log(h)+(yVec-1).dot(numpy.log(1-h)))+param_reg
+
+        #computo gradiente :Alg Back Propagation
+        delta3=h-yVec
+        delta2=delta3.dot(t2)[:,1:]*self.derivadaSigmoide(a2.dot(t1.T))
+
+        deltaAcum=delta2.T.dot(a1)
+        deltaAcum2=delta3.T.dot(a2)
+
+        grad1=1/m*deltaAcum
+        grad2=1/m*deltaAcum2
+
+
+        grad1[:,1:]=grad1[:,1:]*(self.lambda_/m)*t1[:,1:]
+        grad2[:,1:]=grad2[:,1:]*(self.lambda_/m)*t2[:,1:]
+
+        grad=numpy.concatenate([grad1.flatten(),grad2.flatten()])
+        return j,grad
+    def entrenar(self):
+        j_grad=lambda p:self.getCrossedEntropy(p)
+        theta_Inicial=numpy.concatenate([self.theta1.flatten(),self.theta2.flatten()])
+        opciones={'maxiter':100}
+        res=scipy.minimize(j_grad,theta_Inicial,jac=True,method="TNC",options=opciones)
+        thetaOptimos=res.x
+        
+
+
+
+
+
+
+
+r=RedNeuronal()
+r.capa1=400
+r.capa2=25
+r.capa3=10
+r.getCrossedEntropy(numpy.ones(10285))
